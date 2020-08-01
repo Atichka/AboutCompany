17c01ebb9c70f23175ee83a03d5f2d338db0feae
Atichka <atichka@MacBook-Air-Ekaterina.local>
2020-08-01 14:17:22 +0300

    fix button, pic, style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./index.css";
+
+const buttonWork = document.querySelector('.button__work');
+const buttonClose = document.querySelector('.button__close');
+
+function stutusButtonWork() {
+  buttonWork.classList.add("hide");
+  buttonClose.classList.remove("hide");
+}
+
+function stutusButtonClose() {
+  buttonClose.classList.add("hide");
+  buttonWork.classList.remove("hide");
+}
+
+buttonWork.addEventListener('click', stutusButtonWork);
+buttonClose.addEventListener('click', stutusButtonClose);
