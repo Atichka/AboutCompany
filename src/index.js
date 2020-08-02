45903cff28380773bf5ecd0f684e75b7caba6982
Atichka <atichka@MacBook-Air-Ekaterina.local>
2020-08-02 14:33:37 +0300

    add block comment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import "./index.css";
 
 const buttonWork = document.querySelector('.button__work');
 const buttonClose = document.querySelector('.button__close');
+const error = document.querySelector('.comment__error');
+const btnSend = document.querySelector('.comment__button');
+const input = document.querySelector('.comment__input');
+const btnComment = document.querySelector('.look__comment');
+const comment = document.querySelector('.comment');
 
 function stutusButtonWork() {
   buttonWork.classList.add("hide");
@@ -13,5 +18,19 @@ function stutusButtonClose() {
   buttonWork.classList.remove("hide");
 }
 
+function validate() {
+  if(input.textContent==="") {
+    error.textContent="Для оставления отзывов заполните информацию о себе в профиле";
+    error.style="display: block;";
+  }
+}
+
+function openComment() {
+  comment.style=("display: block;");
+}
+
 buttonWork.addEventListener('click', stutusButtonWork);
 buttonClose.addEventListener('click', stutusButtonClose);
+btnSend.addEventListener('click', validate);
+btnComment.addEventListener('click', openComment);
+
